5aa87bd63b23d0b72413fcfd3f4a580607391c9d
unknown <gustavohenriquearagaosilva@gmail.com>
2023-10-08 18:22:23 -0300

    refactor: Refatorando a mecânica de poderes

--- scripts/poderes.js
+++ scripts/poderes.js
@@ -1,5 +1,31 @@
 import { passarVez } from "./actions.js";
 import * as utils from './utils.js';
+import { embaralharMatriz, randint } from './random.js';
+import { tabuleiroGrande} from './main.js';
+
+const embaralharJogoPequeno = (linhaJogoPequeno, colunaJogoPequeno) => {
+  const jogoPequenoJS = tabuleiroGrande[linhaJogoPequeno][colunaJogoPequeno]
+  const jogoPequenoJSEmbaralhado = embaralharMatriz(jogoPequenoJS)
+
+  tabuleiroGrande[linhaJogoPequeno][colunaJogoPequeno] = jogoPequenoJSEmbaralhado
+}
+
+const embaralharJogoPequenoAleatorio = () => {
+  const indexLinhaAleatorio = randint(0, tabuleiroGrande.length - 1)
+  const indexColunaAleatorio = randint(0, tabuleiroGrande.length - 1)
+
+  const jogoSorteado = tabuleiroGrande[indexLinhaAleatorio][indexColunaAleatorio]
+  if (Array.isArray(jogoSorteado)) return embaralharJogoPequeno(indexLinhaAleatorio, indexColunaAleatorio)
+  else return embaralharJogoPequenoAleatorio()
+}
+
+const embaralharTabuleiro = () => {
+  tabuleiroGrande.map((linhaGrande, indexLinhaJogo) => {
+    linhaGrande.map((jogoPequeno, indexColunaJogo) => {
+      if (Array.isArray(jogoPequeno)) return embaralharJogoPequeno(indexLinhaJogo, indexColunaJogo)
+    })
+  })
+}
 
 /**
  * Função para inserir o símbolo em alguma casa que esteja desocupada.
